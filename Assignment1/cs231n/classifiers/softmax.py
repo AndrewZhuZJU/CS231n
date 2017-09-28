ebd05f5f5c38b9206a62207b39dbfd5014859dc9
AndrewZhuZJU <binzhubz@zju.edu.cn>
2017-09-28 21:23:24 +0800

    softmax classifier

---
+++ Assignment1/cs231n/classifiers/softmax.py
@@ -0,0 +1,105 @@
+import numpy as np
+from random import shuffle
+import math
+#from past.builtins import xrange
+
+def softmax_loss_naive(W, X, y, reg):
+  """
+  Softmax loss function, naive implementation (with loops)
+
+  Inputs have dimension D, there are C classes, and we operate on minibatches
+  of N examples.
+
+  Inputs:
+  - W: A numpy array of shape (D, C) containing weights.
+  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+    that X[i] has label c, where 0 <= c < C.
+  - reg: (float) regularization strength
+
+  Returns a tuple of:
+  - loss as single float
+  - gradient with respect to weights W; an array of same shape as W
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+  #scores = np.zeros((num_train,num_classes))
+  #correct_class_scores = []
+  #print(scores.shape)
+  for i in range(num_train):
+    scores = np.dot(X[i],W)
+    correct_class_score = scores[y[i]]
+    #print(math.exp(correct_class_score))
+    denominator = np.sum(np.exp(scores))
+    prob = np.exp(scores)/denominator
+    
+    #x = denominator - correct_class_score
+    loss += -math.log(math.exp(correct_class_score)/denominator)
+    
+    #sigmoid  f'(x) = f(x)*(1-f(x))
+    for j in range(num_classes):
+      if(j==y[i]):
+        dW[:,j] -= (1-prob[j])*X[i]
+      else:
+        #print(prob[j])
+        dW[:,j] += prob[j]*X[i]
+    #dW[i] += (1-math.exp(-x))*(math.exp(-x))
+    #print(x)
+    #print(dW[i]) 
+
+      
+
+  loss /= num_train
+  dW /= num_train
+  loss += reg * np.sum(W*W)
+  dW += 2*reg*W;
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW
+
+
+def softmax_loss_vectorized(W, X, y, reg):
+  """
+  Softmax loss function, vectorized version.
+
+  Inputs and outputs are the same as softmax_loss_naive.
+  """
+  # Initialize the loss and gradient to zero.
+  loss = 0.0
+  dW = np.zeros_like(W)
+
+  #############################################################################
+  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+  # Store the loss in loss and the gradient in dW. If you are not careful     #
+  # here, it is easy to run into numeric instability. Don't forget the        #
+  # regularization!                                                           #
+  #############################################################################
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+  scores = np.dot(X,W)
+  prob = np.exp(scores)/np.sum(np.exp(scores),axis=1,keepdims=True)
+  #print(prob[np.arange(num_train),y].shape)
+
+  loss = - np.sum(np.log(prob[np.arange(num_train),y]))/num_train + reg*np.sum(W*W)
+  prob_mat = np.zeros_like(prob)
+  #pred_class = np.argmax(prob,axis=1)
+  prob_mat[np.arange(num_train),y] = 1
+  dW = -np.dot(X.T, prob_mat-prob)/num_train + 2*reg*W
+  #############################################################################
+  #                          END OF YOUR CODE                                 #
+  #############################################################################
+
+  return loss, dW
+
